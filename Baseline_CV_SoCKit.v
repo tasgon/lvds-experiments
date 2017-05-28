e5610b0dbb15903077aa3a8f5834d11dc6e290c7
tasgoon <tasgo@outlook.com>
2017-05-27 22:54:34 -0400

    not working, but the comments do

--- Baseline_CV_SoCKit.v
+++ Baseline_CV_SoCKit.v
@@ -2,6 +2,8 @@
 //`define ENABLE_HPS
 //`define ENABLE_HSMC
 
+`define CYCLE 1087
+
 module Baseline_CV_SoCKit(
 	//keys and switches
 	input [3:0] KEY,
@@ -18,11 +20,12 @@ module Baseline_CV_SoCKit(
 
 //REG/WIRE declarations
 
-reg [3:0] data = 4'b0000;
+reg [3:0] data = 4'b1111;
 reg [23:0] timer = 24'b0;
-reg [1:0] state = 1'b0;
+reg [23:0] p_duty = 24'b0;
+reg state = 1'b1;
 
-assign LED = HSMC_TX_p;
+assign LED = data;
 
 //Structural coding
 
@@ -39,26 +42,66 @@ assign LED = HSMC_TX_p;
 	end
 end*/
 
-always @(posedge KEY[0]) begin
+/*always @(posedge KEY[0]) begin
 	data <= data + 1'b1;
+	//if (viz) begin LED <= data; end
+end*/
+
+/*always @(posedge KEY[1]) begin
+	cycle <= cycle + 1'b1;
+	//if (!viz) begin LED <= cycle; end
+end
+
+always @(posedge KEY[2]) begin
+	viz <= !viz;
+	case (viz)
+		1'b0: 
+end*/
+
+always @(SW) begin
+	p_duty <= `CYCLE * SW / 16;
 end
 
 always @(state) begin
-	HSMC_TX_p = 4'b0000;
-	HSMC_TX_n = 4'b0000;
+	HSMC_TX_p <= 4'b0000;
+	HSMC_TX_n <= 4'b0000;
 	case (state)
-		1'b0: HSMC_TX_p <= data;
-		1'b1: HSMC_TX_n <= data;
+		1'b0: HSMC_TX_n <= data;
+		1'b1: HSMC_TX_p <= data;
 	endcase
 end
 
 always @(posedge OSC_50_B8A) begin
-	if (timer == 24'b0) begin
+	if (timer > p_duty) begin
+		state = 1'b0;
+	end else begin
+		state = 1'b1;
+	end
+	if (timer == `CYCLE) begin
+			timer <= 24'b0;
+	end
+	timer <= timer + 1'b1;
+	/*if (timer == 24'b0) begin
 		state <= !state;
-		timer <= {SW, 20'b0};
+		//timer <= 544;
+		timer <= 12;
+		
+		case (state)
+			1'b0: begin
+				//timer <= (timer * SW);
+				HSMC_TX_n <= 4'b0000;
+				HSMC_TX_p <= data;
+			end
+			1'b1: begin
+				//timer <= timer * (16 - SW);
+				HSMC_TX_p <= 4'b0000;
+				HSMC_TX_n <= data;
+			end
+		endcase
+		//timer <= timer / 16;
 	end else begin
 		timer <= timer - 1'b1;
-	end
+	end*/
 end
 
 endmodule
---
+++ LVDS_Experiments.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 17.0.0 Build 595 04/25/2017 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(5CSXFC6D6F31) Path("C:/Users/myself/Code/FPGA/lvds-experiments/") File("LVDS_Experiments.sof") MfrSpec(OpMask(1));
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
